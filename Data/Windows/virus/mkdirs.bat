9eb06bc8dd4a0e18971f021d293cac1a0dd48ea1
AmbroseRen <35362717+AmbroseRen@users.noreply.github.com>
2020-07-29 09:56:36 +0800

    Add files via upload

---
+++ Data/Windows/virus/mkdirs.bat
@@ -0,0 +1,51 @@
+@echo off
+md "f:\0000\Google"
+md "f:\0Cache"
+md "f:\0Date\Source"
+md "f:\0Date\SQL\MySQL"
+md "f:\0Date\SQL\Oracle"
+md "f:\0File"
+md "f:\0InstallPackage\Browsers"
+md "f:\0InstallPackage\IDE\Java"
+md "f:\0InstallPackage\IDE\Php"
+md "f:\0InstallPackage\Games"
+md "f:\0InstallPackage\Movies"
+md "f:\0InstallPackage\SoftWare"
+md "f:\0InstallPackage\System"
+md "f:\0InstallPackage\Tools"
+md "f:\0InstallPackage\Zip"
+md "f:\0Plugins"
+md "f:\0SoftWare"
+md "f:\0Tools\bat"
+md "f:\0Tools\virus"
+md "f:\0Zip\Browsers"
+md "f:\0Zip\IDE\Java"
+md "f:\0Zip\IDE\Php"
+md "f:\0Zip\Games"
+md "f:\0Zip\MAVEN"
+md "f:\0Zip\Plugins"
+md "f:\0Zip\Project"
+md "f:\0Zip\SoftWare"
+md "f:\0Zip\System"
+md "f:\0Zip\Tools"
+md "f:\MAVEN\img"
+md "f:\MAVEN\js"
+md "f:\MAVEN\repository"
+md "f:\Music"
+md "f:\Picture\mobile"
+md "f:\Projects"
+md "f:\Template\backup"
+echo ? > f:Template\01.txt
+echo ? > f:Template\02.txt
+echo ? > f:Template\03.txt
+echo ? > f:Template\11.java
+echo ? > f:Template\12.java
+echo ? > f:Template\13.java
+echo ? > f:Template\21.js
+echo ? > f:Template\22.js
+echo ? > f:Template\23.js
+echo ? > f:Template\31.html
+echo ? > f:Template\32.html
+echo ? > f:Template\33.html
+md "f:\Video\movies"
+md "f:\workspace"
---
+++ Data/Windows/virus/mkfiles.bat
@@ -0,0 +1,6 @@
+echo 123 > e:aaa.txt
+echo 456 >> e:aaa.txt
+echo 789 >> e:aaa.txt
+echo 789 >> e:aaa.bat
+
+type tst >> e:aaa.bat
