d6a7143b65fae695d91f572c42ac76145ff467a3
AmbroseRen <35362717+AmbroseRen@users.noreply.github.com>
2020-08-06 09:46:37 +0800

    Create hexo.bat

---
+++ Doc/hexo.bat
@@ -0,0 +1,24 @@
+#Create a new post
+$ hexo new "My New Post"
+
+#Run server
+$ hexo server
+URL:
+http://localhost:4000/
+
+#Generate static files
+$ hexo generate
+
+#Deploy to remote sites--部署到远程站点
+$ hexo deploy
+
+#nodeJs flies
+D:\0Tools\nodejs
+#npm flies
+C:\Users\DELL\AppData\Roaming\npm
+#blog files
+C:\Windows\System32\blog
+_config.yml URL:C:\Windows\System32\blog
+
+#files issue
+C:\Windows\System32\blog\source
