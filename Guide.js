10f9194397b3c488c9b3a49673a5de014f9097d1
AmbroseRen <35362717+AmbroseRen@users.noreply.github.com>
2020-08-19 15:03:38 +0800

    Create Guide.js

---
+++ Guide.js
@@ -0,0 +1,9 @@
+/*引用粒子特效JS*/
+<script id="c_n_script" src="https://blog-static.cnblogs.com/files/xiaokang01/js.js" color="240,230,140" opacity="1" count="75" zindex="-2">
+if(/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
+
+} else {
+
+}
+</script>
+
