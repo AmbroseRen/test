e98e9702672c44f540f5e28d4c598c499d1aacae
AmbroseRen <35362717+AmbroseRen@users.noreply.github.com>
2020-08-19 15:05:33 +0800

    Create Gudie.js

---
+++ js/Gudie.js
@@ -0,0 +1,11 @@
+/*引用粒子特效JS
+https://raw.githubusercontent.com/AmbroseRen/test/master/js/particle.js
+https://blog-static.cnblogs.com/files/xiaokang01/js.js
+*/
+<script id="c_n_script" src="https://raw.githubusercontent.com/AmbroseRen/test/master/js/particle.js" color="240,230,140" opacity="1" count="75" zindex="-2">
+if(/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
+
+} else {
+
+}
+</script>
