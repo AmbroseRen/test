fb2a8f8eff14affa841f86135736ab7799daea29
AmbroseRen <35362717+AmbroseRen@users.noreply.github.com>
2020-08-19 15:14:02 +0800

    Update Gudie.js

--- js/Gudie.js
+++ js/Gudie.js
@@ -1,7 +1,6 @@
 /*引用粒子特效JS
 https://raw.githubusercontent.com/AmbroseRen/test/master/js/particle.js
 https://blog-static.cnblogs.com/files/xiaokang01/js.js
-https://git.hust.cc/canvas-nest.js/
 */
 <script id="c_n_script" src="https://raw.githubusercontent.com/AmbroseRen/test/master/js/particle.js" color="240,230,140" opacity="1" count="75" zindex="-2">
 if(/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
