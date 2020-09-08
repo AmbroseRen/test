77430ae212e6107e48a544f53ba94c5766e3fbef
AmbroseRen <35362717+AmbroseRen@users.noreply.github.com>
2020-09-08 14:54:17 +0800

    Update test.py

--- Data/Python/test.py
+++ Data/Python/test.py
@@ -1,1 +1,19 @@
-# test
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+ 
+year = 2019
+month = 12
+day = 31
+ 
+months = (0,31,59,90,120,151,181,212,243,273,304,334)
+if 0 < month <= 12:
+    sum = months[month - 1]
+else:
+    print ('data error')
+sum += day
+leap = 0
+if (year % 400 == 0) or ((year % 4 == 0) and (year % 100 != 0)):
+    leap = 1
+if (leap == 1) and (month > 2):
+    sum += 1
+print ('it is the %dth day.' % sum)
