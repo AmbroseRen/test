1d939688b5e980113e7ec2d2df0826dab805b96f
AmbroseRen <35362717+AmbroseRen@users.noreply.github.com>
2020-09-08 15:20:50 +0800

    Update test.py

--- Data/Python/test.py
+++ Data/Python/test.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
- 
-year = 2019
-month = 12
-day = 31
+# raw_input replace to intput,it work...
+# ......................................
+
+year = int(input('year:\n'))
+month = int(input('month:\n'))
+day = int(input('day:\n'))
  
 months = (0,31,59,90,120,151,181,212,243,273,304,334)
 if 0 < month <= 12:
