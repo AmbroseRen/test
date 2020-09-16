dc657170aa5ef56b2ebdff917123daf9a87b0306
AmbroseRen <35362717+AmbroseRen@users.noreply.github.com>
2020-09-16 14:25:44 +0800

    test
    Python requests batch test

---
+++ Data/Python/one_img.py
@@ -0,0 +1,19 @@
+import re
+import requests
+
+temp = 'http://wufazhuce.com/one/'
+count = 1
+for i in range(1500, 1580):
+    url = temp + str(i)
+    page = requests.get(url).text
+    reg = re.compile('<img src="(http://.*?)" alt="" />')
+    img_url = re.findall(reg, page)
+    if img_url != []:
+        with open('./{}.jpg'.format(count), 'wb') as file:
+            try:
+                img_data = requests.get(img_url[0]).content
+                file.write(img_data)
+                count += 1
+            except:
+                pass
+print('OK!')
