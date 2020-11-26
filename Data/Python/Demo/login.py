591e94036ae7a37fd93bda1ec310e71f4eb351cd
AmbroseRen <35362717+AmbroseRen@users.noreply.github.com>
2020-11-26 11:10:13 +0800

    Update login.py

--- Data/Python/Demo/login.py
+++ Data/Python/Demo/login.py
@@ -4,8 +4,8 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36',
     }
 data = {
-    'identity':'1056008502@qq.com',
-    'password':'jz_A@632148459',
+    'identity':'账号',
+    'password':'密码',
 }
 #url ='https://www.itjuzi.com/user/login?redirect=&flag=&radar_coupon='
 url ='https://www.itjuzi.com/user-center/account'
