e1c6e5c89b0fca4f45236a74f8d19aba05030fdd
AmbroseRen <35362717+AmbroseRen@users.noreply.github.com>
2020-11-26 14:53:10 +0800

    Create RandomForestClassifier.py

---
+++ Data/Python/scikitlearn/RandomForestClassifier.py
@@ -0,0 +1,8 @@
+# 拟合和预测：估计器基础
+from sklearn.ensemble import RandomForestClassifier
+clf = RandomForestClassifier(random_state=0)
+X = [[ 1,  2,  3],  # 2 samples, 3 features
+     [11, 12, 13]]
+y = [0, 1]  # classes of each sample
+clf.fit(X, y)
+print(clf.fit(X, y))
