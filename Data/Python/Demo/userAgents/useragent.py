43751ec6ea2a9a4819164477f2813c16b61cbc80
AmbroseRen <35362717+AmbroseRen@users.noreply.github.com>
2020-11-30 10:32:57 +0800

    Create useragent.py

---
+++ Data/Python/Demo/userAgents/useragent.py
@@ -0,0 +1,5 @@
+# 列表存储不同的User-Agent,这里仅示例三个,详情可百度百度搜索,链接在代码下方
+ua_list = ['Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.835.163 Safari/535.1',
+            'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:6.0) Gecko/20100101 Firefox/6.0',
+            'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; InfoPath.3)'
+        ]
