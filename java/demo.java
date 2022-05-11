20e594decf204f3c810e41cb2f9cca083c6e7b0d
AmbroseRen <ambroserencn@gmail.com>
2022-05-11 11:40:43 +0800

    Update demo.java

--- java/demo.java
+++ java/demo.java
@@ -1,1 +1,12 @@
-*
+import java.text.SimpleDateFormat;
+import java.util.Date;
+ 
+public class Main{
+    public static void main(String[] args){
+        
+        SimpleDateFormat sdf = new SimpleDateFormat();// 格式化时间 
+        sdf.applyPattern("yyyy-MM-dd HH:mm:ss a");// a为am/pm的标记  
+        Date date = new Date();// 获取当前时间 
+        System.out.println("现在时间：" + sdf.format(date)); // 输出已经格式化的现在时间（24小时制） 
+    } 
+}
