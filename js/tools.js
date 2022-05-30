6f65f6d0f1867acf9ff161fea2382fa73816a7ea
AmbroseRen <ambroserencn@gmail.com>
2022-05-30 16:28:32 +0800

    Update tools.js

--- js/tools.js
+++ js/tools.js
@@ -3,3 +3,13 @@
 **/
 d = d.replace(/\"/g, ""); //js去掉双引号
 
+/**计算代码运行时间*/
+var beginTime = +new Date();
+var endTime = +new Date();
+console.log("排序用时共计"+(endTime-beginTime)+"ms");
+/**Java计算代码运行时间*/
+long startTime=System.currentTimeMillis();   //获取开始时间  
+doSomeThing();  //测试的代码段  
+long endTime=System.currentTimeMillis(); //获取结束时间  
+System.out.println("程序运行时间： "+(end-start)+"ms");
+
