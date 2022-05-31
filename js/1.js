45d95ba9e548e4b15240766a375d7d97ff14c953
AmbroseRen <ambroserencn@gmail.com>
2022-05-31 14:15:13 +0800

    Update 1.js

--- js/1.js
+++ js/1.js
@@ -1,4 +1,5 @@
 https://cdn.jsdelivr.net/gh/AmbroseRen/test@master/js/leaflet/leaflet.canvas-markers.min.js
 
+https://github.com/eJuke/Leaflet.Canvas-Markers/issues/19
 window.L.CanvasIconLayer = layerFactory(L);
 https://github.com/gisarmory/gisarmory.blog/blob/master/leaflet-100ThousandData/demo.html
