948a5246f0ad47d8469e31d9192489503e3c00ba
AmbroseRen <ambroserencn@gmail.com>
2022-07-13 14:59:33 +0800

    Update demo.java

--- java/demo.java
+++ java/demo.java
@@ -9,5 +9,12 @@ public class Main{
         Date date = new Date();// 获取当前时间
         String sDate = sdf.format(date);
         System.out.println("现在时间：" + sDate); // 输出已经格式化的现在时间（24小时制）
+     
+        DateTimeFormatter fmTime = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println("当前时间:"+now.format(fmTime));
+        //2小时前
+        LocalDateTime pro2hour = LocalDateTime.now().minus(2, ChronoUnit.HOURS);
+        System.out.println("2小时前:"+pro2hour.format(fmTime));     
     } 
 }
